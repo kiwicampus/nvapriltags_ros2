d5937269073f146e6437f5fff10fa470caf06684
Sebastian Solarte <johan.solarte@kiwibot.com>
2025-01-17 18:36:41 +0000

    Core code

--- include/AprilTagNode.hpp
+++ include/AprilTagNode.hpp
@@ -7,7 +7,6 @@
  * distribution of this software and related documentation without an express
  * license agreement from NVIDIA CORPORATION is strictly prohibited.
  */
-// clang-format off
 
 #pragma once
 
@@ -15,50 +14,27 @@
 
 #include "nvapriltags_ros2/msg/april_tag_detection.hpp"
 #include "nvapriltags_ros2/msg/april_tag_detection_array.hpp"
+#include <image_transport/image_transport.hpp>
+#include <image_transport/camera_subscriber.hpp>
 #include <rclcpp/rclcpp.hpp>
 #include <sensor_msgs/msg/camera_info.hpp>
 #include <sensor_msgs/msg/image.hpp>
-#include <std_srvs/srv/set_bool.hpp>
 #include <tf2_msgs/msg/tf_message.hpp>
-#include "utils/parameters.hpp"
 
 class AprilTagNode : public rclcpp::Node {
 public:
     AprilTagNode(const rclcpp::NodeOptions options = rclcpp::NodeOptions());
 
 private:
-    void onCameraFrame(const sensor_msgs::msg::Image::ConstSharedPtr& msg_img);
+    void onCameraFrame(const sensor_msgs::msg::Image::ConstSharedPtr& msg_img, const sensor_msgs::msg::CameraInfo::ConstSharedPtr& msg_ci);
    
-    void onCameraInfo(const sensor_msgs::msg::CameraInfo::ConstSharedPtr &msg_ci);
+    const std::string tag_family_;
+    const double tag_edge_size_;
+    const int max_tags_;
 
-    void controlProcessing(const std_srvs::srv::SetBool::Request::SharedPtr request,
-                         std_srvs::srv::SetBool::Response::SharedPtr response);
-
-    void processImages();
-
-    rcl_interfaces::msg::SetParametersResult parameters_cb(const std::vector<rclcpp::Parameter>& parameters);
-
-    std::string tag_family_;
-    double tag_edge_size_;
-    int max_tags_;
-    int throttle_interval_ms_ = 100;
-    bool enable_processing_ = true;
-    bool system_initialized_ = false;
-
-    // const image_transport::CameraSubscriber sub_cam_;
-    rclcpp::Subscription<sensor_msgs::msg::CameraInfo>::SharedPtr sub_cam_info_;
-    rclcpp::Subscription<sensor_msgs::msg::Image>::SharedPtr sub_cam_;
-    rclcpp::CallbackGroup::SharedPtr detection_callback_group_;
-    std::shared_ptr<rclcpp::executors::SingleThreadedExecutor> detection_exec_;
-    rclcpp::TimerBase::SharedPtr processing_timer_;
-
-    rclcpp::Service<std_srvs::srv::SetBool>::SharedPtr srv_enable_processing_;
+    const image_transport::CameraSubscriber sub_cam_;
     const rclcpp::Publisher<tf2_msgs::msg::TFMessage>::SharedPtr pub_tf_;
     const rclcpp::Publisher<nvapriltags_ros2::msg::AprilTagDetectionArray>::SharedPtr pub_detections_;
-    sensor_msgs::msg::CameraInfo::ConstSharedPtr saved_cam_info_;
-
-    NodeParamManager param_manager_;
-    OnSetParametersCallbackHandle::SharedPtr params_callback_handle_;
 
     struct AprilTagsImpl;
     std::unique_ptr<AprilTagsImpl> impl_;
--- src/AprilTagNode.cpp
+++ src/AprilTagNode.cpp
@@ -1,18 +1,8 @@
-/*
- * Copyright (c) 2021, NVIDIA CORPORATION.  All rights reserved.
- *
- * NVIDIA CORPORATION and its licensors retain all intellectual property
- * and proprietary rights in and to this software, related documentation
- * and any modifications thereto.  Any use, reproduction, disclosure or
- * distribution of this software and related documentation without an express
- * license agreement from NVIDIA CORPORATION is strictly prohibited.
- */
-
-// clang-format off
 #include <cv_bridge/cv_bridge.h>
 
 #include <AprilTagNode.hpp>
 #include <Eigen/Dense>
+#include <image_transport/image_transport.hpp>
 
 #include "cuda.h"          // NOLINT
 #include "cuda_runtime.h"  // NOLINT
@@ -119,87 +109,34 @@ struct AprilTagNode::AprilTagsImpl {
 AprilTagNode::AprilTagNode(rclcpp::NodeOptions options)
     : Node("apriltag", "apriltag", options.use_intra_process_comms(true)),
       // parameter
-      // tag_family_(declare_parameter<std::string>("family", "36h11")),
-      // tag_edge_size_(declare_parameter<double>("size", 2.0)),
-      // max_tags_(declare_parameter<int>("max_tags", 20)),
+      tag_family_(declare_parameter<std::string>("family", "36h11")),
+      tag_edge_size_(declare_parameter<double>("size", 2.0)),
+      max_tags_(declare_parameter<int>("max_tags", 20)),
       // topics
-      // sub_cam_(image_transport::create_camera_subscription(
-      //     this, "image",
-      //     std::bind(&AprilTagNode::onCameraFrame, this, std::placeholders::_1,
-      //               std::placeholders::_2),
-      //     declare_parameter<std::string>("image_transport", "raw"),
-      //     rmw_qos_profile_sensor_data)),
+      sub_cam_(image_transport::create_camera_subscription(
+          this, "image",
+          std::bind(&AprilTagNode::onCameraFrame, this, std::placeholders::_1,
+                    std::placeholders::_2),
+          declare_parameter<std::string>("image_transport", "raw"),
+          rmw_qos_profile_sensor_data)),
       pub_tf_(
           create_publisher<tf2_msgs::msg::TFMessage>("/tf", rclcpp::QoS(100))),
       pub_detections_(
           create_publisher<nvapriltags_ros2::msg::AprilTagDetectionArray>(
               "detections", rclcpp::QoS(1))),
-      impl_(std::make_unique<AprilTagsImpl>()) 
-      {
-        param_manager_ = NodeParamManager(this);
-        param_manager_.addParameter<std::string>(tag_family_, "family", "36h11");
-        param_manager_.addParameter<double>(tag_edge_size_, "size", 2.0);
-        param_manager_.addParameter<int>(max_tags_, "max_tags", 20);
-        param_manager_.addParameter<int>(throttle_interval_ms_, "processing_period_ms", 100);
-        params_callback_handle_ = this->add_on_set_parameters_callback(
-          std::bind(&AprilTagNode::parameters_cb, this, std::placeholders::_1));
-
-        detection_callback_group_ = this->create_callback_group(rclcpp::CallbackGroupType::MutuallyExclusive, false);
-        // Add camera info and image subscriptions to the callback group
-        rclcpp::SubscriptionOptions sub_options;
-        sub_options.callback_group = detection_callback_group_;
-        // Create separate subscriptions for camera info and image
-        sub_cam_info_ = create_subscription<sensor_msgs::msg::CameraInfo>(
-            "camera_info", rclcpp::QoS(1).best_effort(), std::bind(&AprilTagNode::onCameraInfo, this, std::placeholders::_1), sub_options);
-
-        sub_cam_ = create_subscription<sensor_msgs::msg::Image>(
-            "image", rclcpp::QoS(1).best_effort(), std::bind(&AprilTagNode::onCameraFrame, this, std::placeholders::_1), sub_options);
-
-        detection_exec_ = std::make_shared<rclcpp::executors::SingleThreadedExecutor>();
-        detection_exec_->add_callback_group(detection_callback_group_, get_node_base_interface());
-
-        processing_timer_ = this->create_wall_timer(
-            std::chrono::milliseconds(throttle_interval_ms_),
-            std::bind(&AprilTagNode::processImages, this));
-
-        // Service to start/stop processing
-        srv_enable_processing_ = create_service<std_srvs::srv::SetBool>(
-            "control_processing",
-            std::bind(&AprilTagNode::controlProcessing, this, std::placeholders::_1, std::placeholders::_2));
-      }
-
-void AprilTagNode::processImages() {
-  if (enable_processing_) {
-    detection_exec_->spin_some();
-  }
-}
+      impl_(std::make_unique<AprilTagsImpl>()) {}
 
 void AprilTagNode::onCameraFrame(
-    const sensor_msgs::msg::Image::ConstSharedPtr &msg_img) {
-  cv::Mat img_rgb8 = cv_bridge::toCvShare(msg_img, "rgb8")->image;
+    const sensor_msgs::msg::Image::ConstSharedPtr &msg_img,
+    const sensor_msgs::msg::CameraInfo::ConstSharedPtr &msg_ci) {
+  // Convert frame to 8-bit RGBA image
+  const cv::Mat img_rgba8 = cv_bridge::toCvShare(msg_img, "rgba8")->image;
 
-  // Create an empty RGBA image with the same size as the input image
-  cv::Mat img_rgba8;
-
-  // Convert the RGB image to RGBA by adding an alpha channel
-  cv::cvtColor(img_rgb8, img_rgba8, cv::COLOR_RGB2RGBA);
-
-  if(saved_cam_info_)
-  {
   // Setup detector on first frame
-    if (impl_->april_tags_handle == nullptr) {
-      impl_->initialize(*this, img_rgba8.cols, img_rgba8.rows,
-                        img_rgba8.total() * img_rgba8.elemSize(), img_rgba8.step,
-                        saved_cam_info_);
-      RCLCPP_INFO(get_logger(), "Apriltag detection initialized on %s, unsubscribing from %s", sub_cam_->get_topic_name(), sub_cam_info_->get_topic_name());
-      // After initialization, stop subscribing to camera info
-      saved_cam_info_.reset();
-      system_initialized_ = true;
-    }
-  }
-  else
-  {
-    RCLCPP_INFO_THROTTLE(get_logger(), *get_clock(), 20000, "Waiting for camera info on topic %s", sub_cam_->get_topic_name());
+  if (impl_->april_tags_handle == nullptr) {
+    impl_->initialize(*this, img_rgba8.cols, img_rgba8.rows,
+                      img_rgba8.total() * img_rgba8.elemSize(), img_rgba8.step,
+                      msg_ci);
   }
 
   // Copy frame into CUDA buffer
@@ -271,53 +208,8 @@ void AprilTagNode::onCameraFrame(
     msg_detections.detections.push_back(msg_detection);
   }
 
-  if(pub_detections_->get_subscription_count())
-  {
-    pub_detections_->publish(msg_detections);
-  }
+  pub_detections_->publish(msg_detections);
   pub_tf_->publish(tfs);
-}
-
-void AprilTagNode::onCameraInfo(const sensor_msgs::msg::CameraInfo::ConstSharedPtr &msg_ci) {
-  saved_cam_info_ = msg_ci;
-}
-
-void AprilTagNode::controlProcessing(const std_srvs::srv::SetBool::Request::SharedPtr request,
-                        std_srvs::srv::SetBool::Response::SharedPtr response) {
-  if(!system_initialized_)
-  {
-    response->success = false;
-    RCLCPP_ERROR(this->get_logger(), "Apriltag detector on %s cannot be paused because its not initalized", sub_cam_->get_topic_name());
-    response->message = "Execution cannot be paused if the system has not initialized!";
-  }                          
-  enable_processing_ = request->data;
-  response->success = true;
-  response->message = enable_processing_ ? "Processing enabled" : "Processing disabled";
-}
-
-rcl_interfaces::msg::SetParametersResult AprilTagNode::parameters_cb(const std::vector<rclcpp::Parameter>& parameters)
-{
-  // Prevent updating parameters while processing
-  auto result = param_manager_.parametersCb(parameters);
-  for (auto& parameter : parameters)
-  {
-    const auto& type = parameter.get_type();
-    const auto& name = parameter.get_name();
-
-    if (type == rclcpp::ParameterType::PARAMETER_INTEGER)
-    {
-      if (name == "processing_period_ms")
-      {
-        throttle_interval_ms_ = parameter.as_int();
-          processing_timer_->reset();
-          processing_timer_ = this->create_wall_timer(
-            std::chrono::milliseconds(throttle_interval_ms_),
-            std::bind(&AprilTagNode::processImages, this));
-      }
-    }
-  }
-
-  return result;
 }
 
 #include <rclcpp_components/register_node_macro.hpp>
